d7dde38f0a6821af98395001eefa86433e1d4f72
Aidan <ajmalanoski@gmail.com>
2021-03-05 15:04:21 -0800

    Updates split.py with Kyle's suggestions -- no longer recursive, uses random.choices with weights

--- split.py
+++ split.py
@@ -4,7 +4,7 @@
 import argparse
 import random
 
-from typing import Iterator, IO, List, Tuple, Union
+from typing import Iterator, IO, List
 
 
 def read_tags(path: str) -> Iterator[List[List[str]]]:
@@ -27,31 +27,6 @@ def read_tags(path: str) -> Iterator[List[List[str]]]:
         yield lines
 
 
-def write_sentence(
-    outs: List[Tuple[IO, int, int]], sentence: List[List[str]]
-) -> int:
-    """
-    Args:
-    outs: List of tuples, each consisting of a file, the count of entries
-        written to that file, and the cutoff for that file.
-    sentence: A sentence as a list of lists of tags, each list of tags being
-        a word
-
-    Returns the index of the file that was written to, or -1 if all the files
-    are "full."
-    """
-    if not outs:
-        return -1
-    index = random.choice(range(len(outs)))
-    if outs[index][1] < outs[index][2]:
-        write_tags(outs[index][0], sentence)
-        return index
-    else:
-        others = outs.copy()
-        del others[index]
-        return write_sentence(others, sentence)
-
-
 def write_tags(wf: IO, tags: List[List[str]]) -> None:
     for line in tags:
         word = " ".join(line)
@@ -60,6 +35,16 @@ def write_tags(wf: IO, tags: List[List[str]]) -> None:
 
 
 def main(args: argparse.Namespace) -> None:
+    # Asserts to make sure that the values for the split make sense
+    assert (
+        0 < args.train_size and args.train_size < 1
+    ), "train_size must be between 0 and 1"
+    assert (
+        0 < args.dev_size and args.dev_size < 1
+    ), "dev_size must be between 0 and 1"
+    assert (
+        0 < args.test_size and args.test_size < 1
+    ), "test_size must be between 0 and 1"
     random.seed(args.seed)
     length = 0
     with open(args.input, "r") as rf:
@@ -67,27 +52,38 @@ def main(args: argparse.Namespace) -> None:
             # We count sentences by counting blank lines.
             if not line.strip():
                 length += 1
-    train_cutoff = round(length * 0.8)
-    dev_cutoff = round(length * 0.1)
-    test_cutoff = dev_cutoff
+    train_cutoff = round(length * args.train_size)
+    dev_cutoff = round(length * args.dev_size)
+    test_cutoff = round(length * args.test_size)
     with open(args.train, "w") as tf, open(args.dev, "w") as df:
         with open(args.test, "w") as ef:
-            # List of filestreams, the count of sentences written to them so
-            # far, and the maximum number of sentences to write to them.
-            outs: List[Tuple[IO, int, int]] = [
-                (tf, 0, train_cutoff),
-                (df, 0, dev_cutoff),
-                (ef, 0, test_cutoff),
-            ]
+            # Dictionary mapping files to a list containing (i) the current
+            # number of sentences written the file and (ii) the total number
+            # of sentences to be written to the file.
+            counts = {
+                tf: [0, train_cutoff],
+                df: [0, dev_cutoff],
+                ef: [0, test_cutoff],
+            }
+            weights = {
+                tf: args.train_size,
+                df: args.dev_size,
+                ef: args.test_size,
+            }
+            outs = [tf, df, ef]
             for sentence in read_tags(args.input):
-                index = write_sentence(outs, sentence)
-                if index >= 0:
-                    outs[index] = (
-                        outs[index][0],
-                        outs[index][1] + 1,
-                        outs[index][2],
-                    )
-                else:
+                wf = random.choices(
+                    outs, weights=[weights[file] for file in outs]
+                )[0]
+                write_tags(wf, sentence)
+                counts[wf][0] += 1  # Increase count by one
+                # If `wf` has reached its quota for sentences, then remove
+                # `wf` from `outs` so that we no longer write to it.
+                if counts[wf][0] >= counts[wf][1]:
+                    outs.remove(wf)
+                # If there is no file left in `outs` to write to, then we are
+                # done.
+                if not outs:
                     return
 
 
@@ -103,5 +99,26 @@ if __name__ == "__main__":
         type=int,
         required=True,
         help="seed for random number generator",
+    )
+    parser.add_argument(
+        "-t",
+        "--train_size",
+        type=float,
+        default=0.8,
+        help="proportion (not percent) of data for training",
+    )
+    parser.add_argument(
+        "-d",
+        "--dev_size",
+        type=float,
+        default=0.1,
+        help="proportion (not percent) of data for development",
+    )
+    parser.add_argument(
+        "-e",
+        "--test_size",
+        type=float,
+        default=0.1,
+        help="proportion (not percent) of data for testing",
     )
     main(parser.parse_args())
